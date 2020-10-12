748ccd33ebbd1f33c80f6dc4f764a31d92e20663
Ashish Kumar <ashishku@thoughtworks.com>
2020-10-12 22:24:55 +0530

    Adds an implementation of Cell as MyCell
    Signed-off-by: Ashish Kumar <ashishku@thoughtworks.com>

---
+++ src/cell.rs
@@ -0,0 +1,77 @@
+use core::cell::UnsafeCell;
+
+struct MyCell<T> {
+    value: UnsafeCell<T>,
+}
+
+impl<T> MyCell<T> where T : Copy {
+    fn new(value: T) -> Self {
+        MyCell {
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    fn get(&self) -> T
+    {
+        unsafe { *self.value.get().clone() }
+    }
+
+    fn set(&self, value: T) {
+        {
+            unsafe { *self.value.get() = value }
+        }
+    }
+
+    fn replace(&self, value: T) -> T {
+        unsafe {
+            let original_value = *self.value.get().clone();
+            *self.value.get() = value;
+            original_value
+        }
+    }
+
+    fn swap(&self, other: &MyCell<T>) {
+        self.set(other.replace(self.get()))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn get_value_of_cell() {
+        let cell = MyCell::new(32);
+        assert_eq!(cell.get(), 32);
+    }
+
+    #[test]
+    fn set_value_in_cell() {
+        let cell = MyCell::new(32);
+
+        assert_eq!(cell.get(), 32);
+        cell.set(35);
+        assert_eq!(cell.get(), 35);
+        cell.set(45);
+        assert_eq!(cell.get(), 45);
+    }
+
+    #[test]
+    fn replace_value_in_cell() {
+        let cell = MyCell::new(32);
+
+        assert_eq!(cell.replace(30), 32);
+        assert_eq!(cell.get(), 30);
+    }
+
+    #[test]
+    fn swap_value_of_cells() {
+        let cell_one = MyCell::new(40);
+        let cell_two = MyCell::new(50);
+
+        cell_one.swap(&cell_two);
+
+        assert_eq!(cell_one.get(), 50);
+        assert_eq!(cell_two.get(), 40);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+mod cell;
+
+#[cfg(test)]
+mod tests {
+    use std::cell::Cell;
+
+    #[test]
+    fn it_works() {
+        let cell  = Cell::new(43);
+        cell.swap(&Cell::new(34));
+        assert_eq!(2 + 2, 4);
+    }
+}
